2ed4970569d65735c92c96ec66d1860047a48f0e
Amadeus Stevenson <amadeus@decoded.com>
2018-07-16 21:53:20 -0400

    Socket.io implementation

--- js/app.js
+++ js/app.js
@@ -2,13 +2,12 @@
 
   GE XLP Dashboard
 
-  (c) Decoded 2017
+  (c) Decoded 2018
 
 */
 
 // Which Edge Device are we reading? Update with your device
 var device = 'xlp-00';
-var charts = {};
 
 // Define sensor data
 // min/max correspond to sensor ranges
@@ -21,58 +20,9 @@ var sensors = {
   'Humidity' : { 'min': 0, 'max': 100, 'color': 'category5', 'avg': 20.1 },
 };
 
-// The gateway uses Stomp for websocket streams
-var gateway = 'https://predix-isk-gateway-iskdev-allen.run.aws-usw02-pr.ice.predix.io/stomp',
-  topic     = '/topic/' + device,
-  ws = new SockJS(gateway),
-  client = Stomp.over(ws);
-
-// Verbose debugging off
-client.debug = null;
-
-var headers = {
-  'reconnection': true,
-  'reconnectionDelay': 1000,
-  'reconnectionDelayMax': 5000,
-  'forceNew': true
-};
-
-// Connect to the gateway
-client.connect(headers, function() {
-  console.log(`Connected to ${gateway}`);
-  client.subscribe(topic, processStream);
-}, function() {
-  console.log('Websocket connection failed. Try reloading the page.');
-  setInterval(function() {
-    for (var sensor in sensors) {
-      charts[sensor].push(generateHistoricalData(sensor));
-    }
-  }, 2000);
-  
-});
-
-// Process incoming data
-function processStream(payload) {
-  // Get array of sensors from gateway
-  var data = JSON.parse(payload.body).body;
-  // Gateway returns arrays of all data
-  data.forEach(function (sensor) {
-    // filter for data we want
-    var timestamp = sensor.datapoints[0][0];
-    var value = sensor.datapoints[0][1].toFixed(1);
-    var re = new RegExp(`-${device}$`);
-    var sensorName = sensor.name.replace(re,'');
-
-    if (sensorName in sensors) {
-      $(`#${sensorName}_value`).html(value);
-      console.log(Date(timestamp), `${sensorName}: ${value}`);
-      charts[sensorName].push([{time: timestamp/1000, y: value}]);
-    } // end checking if valid sensor
-  });
-
-}
-
 // Initialize charts
+var charts = {};
+
 for (var sensor in sensors) {
   charts[sensor] = $(`#${sensor}`).epoch({
     type: 'time.line',
@@ -92,13 +42,28 @@ for (var sensor in sensors) {
 
 }
 
-/* Debugging functions */
+var socket = io.connect('https://ge-xlp-gateway.herokuapp.com/');
+// var socket = io.connect('http://localhost:5000/');
 
-function generateHistoricalData(sensor) {
-  var val = (sensors[sensor].avg) ? sensors[sensor].avg : sensors[sensor].min + (sensors[sensor].max - sensors[sensor].min)/2;
-  val = (1 + 0.05*Math.random()) * val;
-  return [{
-    time: Date.now()/1000,
-    y: val   
-  }];
-}
+socket.on('connect', function () {
+  console.log("Connected to websocket"); // 'G5p5...'
+});
+
+socket.on('disconnect', function () {
+  console.error("Disconnected from websocket");
+});
+
+socket.on(device, function (data) {
+  console.log(`Received data for ${device}`);
+  data.forEach(function (sensor) {
+    // filter for data we want
+    var re = new RegExp(`-${device}$`);
+    var sensorName = sensor.name.replace(re,'');
+
+    if (sensorName in sensors) {
+      $(`#${sensorName}_value`).html(sensor.value);
+      console.log(new Date(sensor.timestamp), `${sensorName}: ${sensor.value}`);
+      charts[sensorName].push([{time: sensor.timestamp/1000, y: sensor.value}]);
+    } // end checking if valid sensor
+  });
+});
